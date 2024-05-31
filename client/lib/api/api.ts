65217855d1684f3bbcda208a7e376f3855a5dec4
Taras Romaniuk <tarasrom25@gmail.com>
2024-05-31 18:18:28 +0300

    valid url of server

--- client/lib/api/api.ts
+++ client/lib/api/api.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
 
-const api = axios.create({ baseURL: "http://localhost:3001/api" });
+const api = axios.create({ baseURL: "https://todo-app-server-snnr.onrender.com/api" });
 
 export default api;
